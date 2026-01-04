66b9e53b1e44d3ce578fc7316aca0e26d587ecbd
Satendra Rajput <satendracoder@gmail.com>
2026-01-04 21:57:13 +0530

    day-2 DSA

---
+++ src/day_2_java_dsa.java
@@ -0,0 +1,12 @@
+public class day_2_java_dsa {
+    public static void main(String[] args) {
+
+        int number = 12;
+
+        if (number % 2 == 0){
+            System.out.println(number + "is Even");
+        } else {
+            System.out.println(number + "is Odd");
+        }
+    }
+}
