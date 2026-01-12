d7c4c80e00b587756af89246522de6e3da018ac8
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-01-12 23:07:42 +0530

    Add palindrome check in Day_004_java_dsaday_10

--- src/january/day_10_java_dsa.java
+++ src/january/day_10_java_dsa.java
@@ -1,1 +1,40 @@
 
+package january;
+
+public class Day_004_java_dsa {
+    public static void main(String[] args) {
+        int number = 121;
+        int original = number;
+        int reverse = 0;
+
+        while (number !=0){
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number/10;
+        }
+
+        if (original==reverse){
+            System.out.println(original + "is a Palindrome number");
+        } else {
+            System.out.println(original + "is not a Palindrome number");
+        }
+    }
+}
+
+
+//For leet code
+//
+//class Solution {
+//    public boolean isPalindrome(int x) {
+//        if (x < 0) return false;
+//        int original = x;
+//        int reverse = 0;
+//
+//        while (x !=0){
+//            int digit = x % 10;
+//            reverse = reverse * 10 + digit;
+//            x = x/10;
+//        }
+//        return original == reverse;
+//    }
+//}
